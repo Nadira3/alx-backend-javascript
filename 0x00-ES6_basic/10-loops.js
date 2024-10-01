cbf8a70b6ca114115f926c9f8ea7f5b940e6a78b
Nadira3 <paitanun35@gmail.com>
2024-10-01 09:15:35 +0100

    used indecOf in for of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0;
   const newArray = [...array];
   for (const arrValue of newArray) {
     const value = arrValue;
-    newArray[idx] = appendString + value;
-    idx += 1;
+    newArray[newArray.indexOf(value)] = appendString + value;
   }
   return newArray;
 }
