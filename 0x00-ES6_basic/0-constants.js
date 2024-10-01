31f0212293a409bf96dcfdf6bca00a42de5d88a7
Nadira3 <paitanun35@gmail.com>
2024-10-01 04:47:45 +0100

    function taskFirst to instantiate variables using const

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,16 @@
+// taskFirst function
+export function taskFirst() {
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
