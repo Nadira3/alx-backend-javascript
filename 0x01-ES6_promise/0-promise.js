427fab98ac9520db71cf715198f6fd911dd3755d
Nadira3 <paitanun35@gmail.com>
2024-10-04 16:23:41 +0100

    reverted to functional code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
 function getResponseFromAPI() {
-  return new Promise();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Response from API');
+      reject(new Error('Rejected response from API'));
+    }, 2000);
+  });
 }
 
 export default getResponseFromAPI;
