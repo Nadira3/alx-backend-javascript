d1a00adcb56b73b9b0e1831a77d23ed6bbf25803
Nadira3 <paitanun35@gmail.com>
2024-11-29 05:56:09 +0100

    task 3 first submission

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should call Utils.calculateNumber with correct arguments', () => {
+        const spy = sinon.spy(Utils, 'calculateNumber');
+
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledWith(spy, 'SUM', 100, 20);
+        spy.restore();
+    });
+});
