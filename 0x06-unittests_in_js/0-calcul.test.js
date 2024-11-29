f4e9f30d8a48c5f6636f8d3971dbafd9ec73079c
Nadira3 <paitanun35@gmail.com>
2024-11-29 05:47:28 +0100

    task 0 first submission

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return the sum of rounded numbers', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1.4, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
