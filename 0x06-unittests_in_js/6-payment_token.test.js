dbbeb6a7b81d4204bea1bb9e41f65d6b6ca9ef42
Nadira3 <paitanun35@gmail.com>
2024-11-29 05:56:52 +0100

    task 6 first submission

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+    it('should resolve with success message', (done) => {
+        getPaymentTokenFromAPI(true).then((response) => {
+            expect(response).to.deep.equal({ data: 'Successful response from the API' });
+            done();
+        });
+    });
+});
