f9d817356c04de6c7740656f069cc489a2527ba0
Nadira3 <paitanun35@gmail.com>
2024-11-29 05:56:37 +0100

    task 5 first submission

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(() => {
+        consoleSpy.restore();
+    });
+
+    it('should log correct total for (100, 20)', () => {
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledWith(consoleSpy, 'The total is: 120');
+    });
+
+    it('should log correct total for (10, 10)', () => {
+        sendPaymentRequestToApi(10, 10);
+        sinon.assert.calledWith(consoleSpy, 'The total is: 20');
+    });
+});
