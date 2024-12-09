d7960df5a01c867a3ff69c222ddbc34c35291def
Nadira3 <paitanun35@gmail.com>
2024-12-09 04:54:54 +0100

    task 2 first submission

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+// function that readFileSync
+const fs = require('fs');
+
+function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '' && !line.startsWith('firstname'));
+    const students = lines.map(line => line.split(','));
+    const totalStudents = students.length;
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    const fields = {};
+    students.forEach(student => {
+      const field = student[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student[0]);
+    });
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
