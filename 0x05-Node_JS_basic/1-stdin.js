b66e4624acadca9900f7e64db5d5013f24c08e2e
Nadira3 <paitanun35@gmail.com>
2024-12-09 04:54:29 +0100

    task 1 first submission

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// Display initial prompt
+process.stdout.write('Welcome to ALX, what is your name?\n');
+
+// Listen for user input
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim(); // Convert input to string and remove extra spaces
+  console.log(`Your name is: ${name}`);
+  process.stdin.pause(); // Stop listening for input
+});
+
+// Handle program exit
+process.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  process.exit(); // Exit the program
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
